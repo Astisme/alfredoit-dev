817151b750e1a387160db6e4d4bdb58ec4ff7a9c
alfredo <alum.banyans0w@icloud.com>
2022-12-08 18:22:38 +0100

    first fix for deno's internal server error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import deno from "@astrojs/deno";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
   // site: "https://www.alfredoit.dev",
   site: "http://localhost:3000",
+  start: true
 });
