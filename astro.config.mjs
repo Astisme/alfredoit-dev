dc79ed79fcd237a00fe8418ba2773e567af1da55
alfredo <alum.banyans0w@icloud.com>
2022-12-18 16:28:54 +0100

    tried reinstalling node_modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import deno from "@astrojs/deno";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: deno(),
-  site: "https://www.alfredoit.dev",
-  //site: "http://localhost:3000",
-  //start: true
+  site: "https://www.alfredoit.dev"
 });
