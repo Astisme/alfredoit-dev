b19b41c86efb209712bc037e1b6f401f410b976b
Astisme <alum.banyans0w@icloud.com>
2023-03-02 08:54:31 +0100

    imported languages

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,4 @@ export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const WEBSITE_NAME = "AlfredoIT";
 export const FAVICON = "alfred.jpeg";
+export const LANGS = ["it", "en"];
