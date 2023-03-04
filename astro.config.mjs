22da39b942e8b8c423d5e4de4b550f695a935017
Astisme <alum.banyans0w@icloud.com>
2023-03-04 12:38:17 +0100

    started about me

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import deno from "@astrojs/deno";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: deno()
+  adapter: deno(),
+  integrations: [mdx()]
 });
