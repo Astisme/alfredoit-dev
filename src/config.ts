a0007769eadc40596f474a3eec474bf5f86f6f22
Astisme <alum.banyans0w@icloud.com>
2023-03-18 10:36:20 +0100

    theme selection

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const WEBSITE_NAME = "AlfredoIT";
-export const FAVICON = "alfred.jpeg";
+export const FAVICON = "/alfred.jpeg";
 export const LANGS = ["it", "en"];
-export const LOGOS_DIM = "5%";
+export const LOGOS_DIM = "2rem";
