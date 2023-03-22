37040c23debec82f000817c5afed358091a31e5d
Astisme <alum.banyans0w@icloud.com>
2023-03-22 13:24:20 +0100

    added ability to switch between languages

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,4 @@
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const WEBSITE_NAME = "AlfredoIT";
 export const FAVICON = "/alfred.jpeg";
-export const LANGS = ["it", "en"];
-export const LOGOS_DIM = "2rem";
+//export const LANGS = ["it", "en"];
+export const WEBSITE_URL = "alfredoit.dev/";
