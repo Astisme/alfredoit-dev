1166024e8405ae38934ac106614ba162f0ae8eac
Astisme <alum.banyans0w@icloud.com>
2023-07-16 21:00:53 +0200

    improved mobile for landing page

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@ export const WEBSITE_NAME = "AlfredoIT";
 export const FAVICON = "/alfred.jpeg";
 export const WEBSITE_URL = "alfredoit.dev/";
 //export const MOBILE_PX = "768px";
+//export const IPHONESE_PX = "320px";
