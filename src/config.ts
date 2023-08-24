f69f6830771f07fa281553801635a9c683fa0100
Astisme <alum.banyans0w@icloud.com>
2023-08-24 18:00:41 +0200

    logo updates

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const WEBSITE_NAME = "AlfredoIT";
-export const FAVICON = "/alfred.jpeg";
+export const FAVICON = "/alfredit.jpeg";
 export const WEBSITE_URL = "alfredoit.dev/";
 //export const MOBILE_PX = "768px";
 //export const IPHONESE_PX = "320px";
