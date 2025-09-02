b06c01b015db3cd78adbe085c7be5aa5845348d0
Alfredo <a.chissotti@reply.it>
2025-09-02 11:08:16 +0200

    remove ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import deno from "@deno/astro-adapter";
 import mdx from "@astrojs/mdx";
-//import php from "astro-php";
-
 
 export default defineConfig({
-  output: "server",
-  adapter: deno(),
+  output: "static",
   integrations: [
-    //php(),
     mdx()
   ]
 });
