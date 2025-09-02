bd5d8551cfde3c75e60bbdcabfcf16da00877516
Alfredo <a.chissotti@reply.it>
2025-09-02 12:21:43 +0200

    set site on config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  output: "static",
-  integrations: [
-    mdx()
-  ]
+    site: "https://alfredoit.dev",
+    integrations: [
+        mdx()
+    ]
 });
