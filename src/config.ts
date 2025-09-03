93f178385f32600ff2dc4e5ff6b6bf7176f5e220
Alfredo <a.chissotti@reply.it>
2025-09-03 12:15:10 +0200

    created getCurrentLocale

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const WEBSITE_NAME = "AlfredoIT";
 export const FAVICON = "/alfredit.jpeg";
 export const WEBSITE_URL = "alfredoit.dev/";
+export const REPO_URL = "https://github.com/Astisme/alfredoit-dev";
 //export const MOBILE_PX = "768px";
 //export const IPHONESE_PX = "320px";
