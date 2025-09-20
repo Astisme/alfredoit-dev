5870ccddeccf5cd4acb73b0c75e8a7ea01d109bb
Astisme <tyir1d4e@duck.com>
2025-09-21 00:39:13 +0200

    added sponsor page (#27)
    Co-authored-by: Alfredo <a.chissotti@reply.it>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://alfredoit.dev",
   integrations: [
-    mdx(),
+      mdx(),
+      sitemap({
+          filter: (page) => !page.includes('sponsor'),
+          i18n: {
+            defaultLocale: 'en',
+            locales: {
+              en: 'en',
+              it: 'it',
+            }
+          },
+      })
   ],
   i18n: {
     locales: ["en", "it"],
@@ -14,7 +25,7 @@ export default defineConfig({
       prefixDefaultLocale: true,
       fallbackType: "rewrite",
     },
-    falback: {
+    fallback: {
       it: "en",
     },
   },
