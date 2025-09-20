d9781fb58cc5d67f7e804c4c703b68fd1bda33e5
Alfredo <a.chissotti@reply.it>
2025-09-21 00:33:17 +0200

    added sponsor page

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,6 @@ export const WEBSITE_URL = "alfredoit.dev/";
 export const REPO_URL = "https://github.com/Astisme/alfredoit-dev";
 //export const MOBILE_PX = "768px";
 //export const IPHONESE_PX = "320px";
+export const VALID_EMAILS_REGEX =
+  /(againwhysalesforce@duck\.com|.*@alfredoit.dev)/i;
+export const DEFAULT_EMAIL = "info@alfredoit.dev";
