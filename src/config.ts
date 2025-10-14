03673f4d5d1a4687646092cdb3173f43458c839c
Alfredo <a.chissotti@reply.it>
2025-10-14 17:20:21 +0200

    better email regex handling

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const REPO_URL = "https://github.com/Astisme/alfredoit-dev";
 //export const MOBILE_PX = "768px";
 //export const IPHONESE_PX = "320px";
 export const VALID_EMAILS_REGEX =
-  /(againwhysalesforce@duck\.com|.*@alfredoit.dev)/i;
+  /^(againwhysalesforce@duck\.com|.*@alfredoit.dev)$/i;
 export const DEFAULT_EMAIL = "info@alfredoit.dev";
 export const SUPPORTED_LOCALES = ["en","it"];
 export const DEFAULT_LOCALE = SUPPORTED_LOCALES[0];
